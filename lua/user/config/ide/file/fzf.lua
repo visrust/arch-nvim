68bfd06f8228e14d6f28ecca7660b39cefa9b471
visrust <madharchodambani@gmail.com>
2026-01-28 18:40:06 +0530

    fixed fzf themeing

--- lua/user/config/ide/file/fzf.lua
+++ lua/user/config/ide/file/fzf.lua
@@ -17,6 +17,28 @@ vim.defer_fn(function()
                 flip_columns = 120,
             },
         },
+
+        fzf_colors = {
+            -- FORCE main text to use Normal
+            ['fg']      = { 'fg', 'Normal' },
+            ['bg']      = { 'bg', 'Normal' },
+
+            -- Selected item
+            ['fg+']     = { 'fg', 'Normal' },
+            ['bg+']     = { 'bg', 'Visual' },
+
+            -- Matches (strong contrast)
+            ['hl']      = { 'fg', 'Identifier' },
+            ['hl+']     = { 'fg', 'Statement' },
+
+            -- UI
+            ['prompt']  = { 'fg', 'Keyword' },
+            ['pointer'] = { 'fg', 'Type' },
+            ['marker']  = { 'fg', 'Type' },
+            ['header']  = { 'fg', 'Title' },
+            ['info']    = { 'fg', 'Special' },
+        },
+
         files = {
             prompt = ' ',
             multiprocess = true,
@@ -39,6 +61,13 @@ vim.defer_fn(function()
         },
     })
 end, 150) -- Defer by 150ms
+
+vim.api.nvim_set_hl(0, 'FzfLuaNormal', { link = 'Normal' })
+vim.api.nvim_set_hl(0, 'FzfLuaBorder', { link = 'FloatBorder' })
+vim.api.nvim_set_hl(0, 'FzfLuaTitle', { link = 'Title' })
+vim.api.nvim_set_hl(0, 'FzfLuaPreviewNormal', { link = 'Normal' })
+vim.api.nvim_set_hl(0, 'FzfLuaCursorLine', { link = 'Visual' })
+vim.api.nvim_set_hl(0, 'FzfLuaCursor', { link = 'IncSearch' })
 
 -- <leader>tc - Find Neovim config files
 vim.keymap.set('n', '<leader>tc', function()
@@ -64,12 +93,12 @@ vim.keymap.set('n', '<leader>fc', function()
     })
 end, { desc = 'Grep in Neovim config' })
 
-vim.keymap.set('n' , '<leader>ft', '<cmd>FzfLua diagnostics_workspace<cr>',{
-    desc = "Document Dignostic"
+vim.keymap.set('n', '<leader>ft', '<cmd>FzfLua diagnostics_workspace<cr>', {
+    desc = 'Document Dignostic'
 })
 
-vim.keymap.set('n' , '<leader>fz', '<cmd>FzfLua<cr>',{
-    desc = "Document Dignostic"
+vim.keymap.set('n', '<leader>fz', '<cmd>FzfLua<cr>', {
+    desc = 'Document Dignostic'
 })
 
 
@@ -106,36 +135,36 @@ end, { desc = 'Git status' })
 -- Automatically detects Termux, standard Linux, or other systems
 
 local function get_system_home()
-  -- Check if running in Termux
-  if vim.fn.getenv("TERMUX_VERSION") ~= vim.NIL then
-    return "/data/data/com.termux/files/home"
-  end
-  
-  -- Check if PREFIX exists (another Termux indicator)
-  local prefix = vim.fn.getenv("PREFIX")
-  if prefix ~= vim.NIL and prefix:match("com%.termux") then
-    return "/data/data/com.termux/files/home"
-  end
-  
-  -- For standard Linux/Unix systems, use $HOME
-  local home = vim.fn.getenv("HOME")
-  if home ~= vim.NIL then
-    return home
-  end
-  
-  -- Fallback to current directory
-  return vim.fn.getcwd()
+    -- Check if running in Termux
+    if vim.fn.getenv('TERMUX_VERSION') ~= vim.NIL then
+        return '/data/data/com.termux/files/home'
+    end
+
+    -- Check if PREFIX exists (another Termux indicator)
+    local prefix = vim.fn.getenv('PREFIX')
+    if prefix ~= vim.NIL and prefix:match('com%.termux') then
+        return '/data/data/com.termux/files/home'
+    end
+
+    -- For standard Linux/Unix systems, use $HOME
+    local home = vim.fn.getenv('HOME')
+    if home ~= vim.NIL then
+        return home
+    end
+
+    -- Fallback to current directory
+    return vim.fn.getcwd()
 end
 
 local function get_system_root()
-  -- Check if running in Termux
-  if vim.fn.getenv("TERMUX_VERSION") ~= vim.NIL or 
-     (vim.fn.getenv("PREFIX") ~= vim.NIL and vim.fn.getenv("PREFIX"):match("com%.termux")) then
-    return "/data/data/com.termux/files"
-  end
-  
-  -- For standard systems, use root
-  return "/"
+    -- Check if running in Termux
+    if vim.fn.getenv('TERMUX_VERSION') ~= vim.NIL or
+      (vim.fn.getenv('PREFIX') ~= vim.NIL and vim.fn.getenv('PREFIX'):match('com%.termux')) then
+        return '/data/data/com.termux/files'
+    end
+
+    -- For standard systems, use root
+    return '/'
 end
 
 -- Setup fzf.lua keymaps
@@ -143,44 +172,43 @@ local fzf = require('fzf-lua')
 
 -- Keymap: Search files in system home
 vim.keymap.set('n', '<leader>fih', function()
-  local home = get_system_home()
-  fzf.files({ cwd = home })
+    local home = get_system_home()
+    fzf.files({ cwd = home })
 end, { desc = 'Find files in system home' })
 
 -- Keymap: Search all files from system root (use with caution)
 vim.keymap.set('n', '<leader>fir', function()
-  local root = get_system_root()
-  fzf.files({ cwd = root })
+    local root = get_system_root()
+    fzf.files({ cwd = root })
 end, { desc = 'Find files from system root' })
 
 -- Keymap: Grep in system home
 vim.keymap.set('n', '<leader>fgh', function()
-  local home = get_system_home()
-  fzf.live_grep({ cwd = home })
+    local home = get_system_home()
+    fzf.live_grep({ cwd = home })
 end, { desc = 'Live grep in system home' })
 
 -- Keymap: Grep in system root
 vim.keymap.set('n', '<leader>fgr', function()
-  local root = get_system_root()
-  fzf.live_grep({ cwd = root })
+    local root = get_system_root()
+    fzf.live_grep({ cwd = root })
 end, { desc = 'Live grep in system root' })
 
 -- Keymap: Show system info
 vim.keymap.set('n', '<leader>fgi', function()
-  local home = get_system_home()
-  local root = get_system_root()
-  local is_termux = vim.fn.getenv("TERMUX_VERSION") ~= vim.NIL
-  
-  local msg = string.format(
-    "System Info:\n" ..
-    "Environment: %s\n" ..
-    "Home: %s\n" ..
-    "Root: %s",
-    is_termux and "Termux" or "Standard Linux",
-    home,
-    root
-  )
-  
-  vim.notify(msg, vim.log.levels.INFO)
-end, { desc = 'Show system information' })
+    local home = get_system_home()
+    local root = get_system_root()
+    local is_termux = vim.fn.getenv('TERMUX_VERSION') ~= vim.NIL
 
+    local msg = string.format(
+        'System Info:\n' ..
+        'Environment: %s\n' ..
+        'Home: %s\n' ..
+        'Root: %s',
+        is_termux and 'Termux' or 'Standard Linux',
+        home,
+        root
+    )
+
+    vim.notify(msg, vim.log.levels.INFO)
+end, { desc = 'Show system information' })
